5605430a902c33a2d1b05b5662368d4a9468ed69
Anoop Santhanam <anoop.santhanam@gmail.com>
2021-08-19 14:36:32 +0530

    Rendered comments

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { CommentType } from '../../services/types';
+
+const CommentContainer = styled.div`
+  width: 100%;
+  background-color: #eee;
+  padding: 8px;
+  margin-bottom: 8px;
+`;
+
+const Timestamp = styled.div`
+  color: #666;
+  font-size: 12px;
+  margin-top: 16px;
+`;
 
 interface Props {
   comment: CommentType;
 }
 
 export const Comment = ({ comment }: Props) => {
-  const { content, updated_at, user_avatar, username } = comment;
+  const { content, updated_at, username } = comment;
+  const updatedAt = new Date(updated_at);
 
-  return (<div />);
+  return (<CommentContainer>
+    <strong>{username}</strong><br /><br />
+    {content}
+    <Timestamp>Updated on {updatedAt.getDate()}/{updatedAt.getMonth() + 1}/{updatedAt.getFullYear()}</Timestamp>
+  </CommentContainer>);
 };
